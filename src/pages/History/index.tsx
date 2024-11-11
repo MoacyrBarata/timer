bbfed40985ae1493b248fff9a98943236d70c15a
Moacyr Barata <moacyrbarata@gmail.com>
2024-11-11 17:27:56 -0400

    history page table style

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,3 +1,49 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
 export function History(){
-  return <h1>History</h1>
+  return (
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluido</td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Há dois meses</td>
+              <td>Concluido</td>
+            </tr>
+          </tbody>
+        </table>
+        
+      </HistoryList>
+    </HistoryContainer>
+  )
 }
